148ffacf57141d473fef5cce267676d06bb0f3a6
Mehr-creates <64961841+Mehr-creates@users.noreply.github.com>
2023-01-27 13:43:01 +1000

    unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-         Sterco Globals testing
-         Hello hello hello
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
